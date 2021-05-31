8afe19a48bb948f08aa3c613fc41302a9464c403
Richard Carl Mozer <r_carl1997@hotmail.com>
2021-05-31 16:45:21 -0300

    update

--- src/App.js
+++ src/App.js
@@ -3,52 +3,19 @@ import { Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Content from "./components/pages/Content";
 import background from "./img/pokemon-1-background-25.jpg";
+import React from "react";
 
 function App() {
   return (
     <div
       style={{
         backgroundImage: `url(${background})`,
-        backgroundSize: "cover",
+        height: "100vh",
       }}
     >
       <div>
         <Route exact path="/" component={Home} />
         <Route exact path="/content" component={Content} />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
       </div>
     </div>
   );

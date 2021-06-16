ec8ffa52eb1e1a2a463da6e5205be9ea4052c4f0
Richard Carl Mozer <r_carl1997@hotmail.com>
2021-06-16 16:59:14 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
+
 import { Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Content from "./components/pages/Content";
 import background from "./img/pokemon-1-background-25.jpg";
 import React from "react";
+import FooterPage from "./components/FooterPage";
 
 function App() {
   return (
     <div
       style={{
         backgroundImage: `url(${background})`,
-        height: "100vh",
+        height: "90vh",
       }}
     >
       <div>
         <Route exact path="/" component={Home} />
         <Route exact path="/content" component={Content} />
+        <FooterPage />
       </div>
     </div>
   );
